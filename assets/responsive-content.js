65df0b0d26c63078d7ce3cd3c80e30c845ab5817
dimasm93 <software.dimas_m@icloud.com>
2021-01-31 13:07:41 +0700

    update fixing image zoomer

--- assets/responsive-content.js
+++ assets/responsive-content.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-    $('.materialboxed').materialbox();
-    $(".dropdown-button").dropdown();
-    $('.scrollspy').scrollSpy();
-    $(".button-collapse").sideNav({
-        menuWidth: 400
-    });
-    $('.collapsible').collapsible();
-    $('.modal').modal({
-        dismissible: false
-    });
-    $('.tooltipped').tooltip({ delay: 50 });
-    $('.post-body img').addClass('materialboxed').addClass('responsive-img');
+    $('#article-body img').addClass('materialboxed').addClass('responsive-img');
     $('.post-body table').addClass('striped').addClass('highlight').addClass('responsive-table');
-
 });
