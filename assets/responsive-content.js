fabc66b254427ce833f3468d35527e2a6d5d1138
dimasm93 <software.dimas_m@icloud.com>
2021-02-05 20:43:23 +0700

    update layout

--- assets/responsive-content.js
+++ assets/responsive-content.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     $('#article-body img').addClass('materialboxed').addClass('responsive-img');
-    $('.post-body table').addClass('striped').addClass('highlight').addClass('responsive-table');
+    $('.post-body table').not('pre > code > table').addClass('striped').addClass('highlight').addClass('responsive-table');
 });
